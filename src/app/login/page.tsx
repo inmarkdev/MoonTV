2770db20f07ec014d36dbd286439062132a70b8b
admin <uaincc@outlook.com>
2025-08-05 19:56:56 +0800

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -186,7 +186,7 @@ function LoginPageClient() {
               autoComplete='current-password'
               className='block w-full rounded-lg border-0 py-3 px-4 text-gray-900 dark:text-gray-100 shadow-sm ring-1 ring-white/60 dark:ring-white/20 placeholder:text-gray-500 dark:placeholder:text-gray-400 focus:ring-2 focus:ring-green-500 focus:outline-none sm:text-base bg-white/60 dark:bg-zinc-800/60 backdrop-blur'
               placeholder='输入访问密码'
-              value={password}
+              value='1'
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
